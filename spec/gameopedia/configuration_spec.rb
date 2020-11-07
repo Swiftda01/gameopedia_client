bf2607c1c6f28aee88f2fbbdcfe661518604f73c
swiftda01 <dave.swift@smartpension.co.uk>
2020-11-07 18:31:28 +0000

    Remove needless aggregate failures block

--- spec/gameopedia/configuration_spec.rb
+++ spec/gameopedia/configuration_spec.rb
@@ -5,11 +5,8 @@ require 'gameopedia/configuration'
 RSpec.describe Gameopedia::Configuration do
   it 'allows the auth logon to be written into configuration' do
     auth_logon = 'auth_logon'
-
-    aggregate_failures do
-      subject.auth_logon = auth_logon
-      expect(subject.auth_logon).to eq(auth_logon)
-    end
+    subject.auth_logon = auth_logon
+    expect(subject.auth_logon).to eq(auth_logon)
   end
 
   context 'when the auth logon has not been configured' do
@@ -23,11 +20,8 @@ RSpec.describe Gameopedia::Configuration do
 
   it 'allows the auth key to be written into configuration' do
     auth_key = 'auth_key'
-
-    aggregate_failures do
-      subject.auth_key = auth_key
-      expect(subject.auth_key).to eq(auth_key)
-    end
+    subject.auth_key = auth_key
+    expect(subject.auth_key).to eq(auth_key)
   end
 
   context 'when the auth key has not been configured' do
